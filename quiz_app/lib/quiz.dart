795c5d277ebbbe1f2f4a21fd198f92ca87b1c660
mahmudul hasan nihad <mahmudulhasannihad@gmail.com>
2024-10-04 18:23:24 +0600

    Quiz app

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
@@ -13,7 +14,19 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   Widget? activeScreen;
+
+  void chooseAnswer(answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = StartScreen(switchScreen);
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -23,7 +36,7 @@ class _QuizState extends State<Quiz> {
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = QuestionsScreen(onSelectAnswer: chooseAnswer);
     });
   }
 
